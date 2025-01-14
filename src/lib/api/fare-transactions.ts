2a5fceb19296bbfecd22293521b10b967e62df9d
Eugine Makori <92178483+eugo254@users.noreply.github.com>
2025-01-14 14:49:23 +0300

    Update fare-transactions.ts

--- src/lib/api/fare-transactions.ts
+++ src/lib/api/fare-transactions.ts
@@ -1,4 +1,4 @@
-import { supabase } from '../supabase';
+import { supabase, handleSupabaseError } from '../supabase';
 import type { Location, FareTransaction } from '../../types';
 
 export async function saveFareTransaction(
@@ -36,7 +36,36 @@ export async function getFareTransactions(): Promise<FareTransaction[]> {
     .from('fare_transactions')
     .select('*')
     .order('created_at', { ascending: false });
-
   if (error) throw error;
   return data;
+}
+
+export async function getFareTransactionsIds(): Promise<string[]> {
+  const { data, error } = await supabase
+    .from('fare_transactions')
+    .select('id')  // Fetch only the 'id' column to optimize the query
+    .order('created_at', { ascending: false });
+
+  if (error) throw error;
+
+  // Return an array of IDs
+  return data.map(transaction => transaction.id);
+}
+
+
+export async function deleteFareTransactions(ids: string[]): Promise<Boolean> {
+  try {
+    const { error } = await supabase
+      .from('fare_transactions')
+      .delete().in('id', ids);;
+
+    if (error) {
+      handleSupabaseError(error);
+      return false;
+    }
+    return true;
+  } catch (error) {
+    handleSupabaseError(error);
+    return false;
+  }
 }
